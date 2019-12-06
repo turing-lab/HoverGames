3a4774412cdf4ca67852324446e0231de974ee54
Andrés Castellanos <andres.cv@galileo.edu>
2019-12-06 15:48:55 -0600

    fix lepton script

--- lepton/read.py
+++ lepton/read.py
@@ -4,15 +4,17 @@ from pylepton import Lepton
 
 
 def main():
-    img, _ = l.capture()
-    # extend contrast
-    cv2.normalize(img, img, 0, 65535, cv2.NORM_MINMAX)
-    # fit data into 8 bits
-    np.right_shift(img, 8, img)
-    # conver np array to uint8 type
-    img = np.uint8(img)
-    # create an output image
-    cv2.imwrite("out.jpg", img)
+    with Lepton() as l:
+        # get image
+        img, _ = l.capture()
+        # extend contrast
+        cv2.normalize(img, img, 0, 65535, cv2.NORM_MINMAX)
+        # fit data into 8 bits
+        np.right_shift(img, 8, img)
+        # conver np array to uint8 type
+        img = np.uint8(img)
+        # create an output image
+        cv2.imwrite("out.jpg", img)
 
 
 if __name__ == '__main__':
